c2c5b7cdac43c3eb81c680d012fe98debe1427ce
Saran Vellanki <sarankumarvellanki@gmail.com>
2015-05-01 14:58:19 -0700

    add modules

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 let express = require('express')
-let nodeify = require('bluebird')
+let nodeify = require('bluebird-nodeify')
+let morgan = require('morgan')
+
+// songbird works during runtime. Hence, not assigning it to a variable
+require('songbird')
+
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 8000;
+// need a root directory for the file system api implementation
+const ROOT_DIR = process.cwd();
+
+// setting up the express app
+let app = express()
+
+if(NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
