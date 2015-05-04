b5f4c3e1d2e8f5fef0dbee3205f80d8b7252623b
Saran Vellanki <sarankumarvellanki@gmail.com>
2015-05-04 11:59:18 -0700

    add HEAD and sendHeaders

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let path = require('path')
 let express = require('express')
 let nodeify = require('bluebird-nodeify')
 let morgan = require('morgan')
+let mime = require('mime-types')
 
 // songbird works during runtime. Hence, not assigning it to a variable
 require('songbird')
@@ -23,12 +24,22 @@ if(NODE_ENV === 'development') {
 
 app.listen(PORT, () => console.log(`Listening @ http://127.0.0.1:${PORT}`))
 
-app.get('*', (req, res, next) => {
-  
-  
-  async () => {
+app.get('*', sendHeaders, (req, res) => {
+  if (res.body) {
+    res.json(res.body)
+    return
+  }
+  fs.createReadStream(req.filePath).pipe(res)
+})
+
+app.head('*', sendHeaders, (req, res) => res.end())
+
+function sendHeaders(req, res, next) {
+  // promise is connected to the next callback (use of nodeify)
+  nodeify(async () => {
     // ROOT_DIR and __dirname are the same
     let filePath = path.resolve(path.join(ROOT_DIR, req.url))
+    req.filePath = filePath
     if(filePath.indexOf(ROOT_DIR) !== 0) {
       res.send(400, 'Invalid Path')
       return
@@ -37,11 +48,15 @@ app.get('*', (req, res, next) => {
     let stat = await fs.promise.stat(filePath)
     if(stat.isDirectory()) {
       let files = await fs.promise.readdir(filePath)
-      res.json(files)
+      res.body = JSON.stringify(files)
+      res.setHeader('Content-Length', res.body.length)
+      res.setHeader('Content-Type', 'application/json')
       return 
     }
-    fs.createReadStream(filePath).pipe(res)
-  }().catch(next) //next is called if this promise results in an error
-  
-  
-})
+    
+    res.setHeader('Content-Length', stat.size)
+    let contentType = mime.contentType(path.extname(filePath))
+    res.setHeader('Content-Type', contentType)   
+  }(), next)
+  //.catch(next) //next is called if this promise results in an error
+}
