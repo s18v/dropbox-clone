82297811583a4a128ee93e7133349c913c99b640
Saran Vellanki <sarankumarvellanki@gmail.com>
2015-05-04 16:12:00 -0700

    add PUT and POST

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let nodeify = require('bluebird-nodeify')
 let morgan = require('morgan')
 let mime = require('mime-types')
 let rimraf = require('rimraf')
+let mkdirp = require('mkdirp')
 // songbird works during runtime. Hence, not assigning it to a variable
 require('songbird')
 
@@ -33,6 +34,47 @@ app.get('*', setFileMeta, sendHeaders, (req, res) => {
 })
 
 app.head('*', setFileMeta, sendHeaders, (req, res) => res.end())
+
+app.delete('*', setFileMeta, (req, res, next) => {
+  async () => {
+    if (!req.stat) return res.send(400, 'Invalid Path')
+    
+    if (req.stat.isDirectory()) {
+     await rimraf.promise(req.filePath)
+    } else await fs.promise.unlink(req.filePath)
+    res.end()
+  }().catch(next) 
+})
+
+
+app.put('*', setFileMeta, setDirDetails, (req, res, next) => {
+  async () => {
+    if (req.stat) return res.send(405, 'File Exists')
+    await mkdirp.promise(req.dirPath)
+    if(!req.isDir) req.pipe(fs.createWriteStream(req.filePath))
+    res.end()
+  }().catch(next)
+})
+
+app.post('*', setFileMeta, setDirDetails, (req, res, next) => {
+  async () => {
+    if (!req.stat) return res.send(405, 'File does not exist')
+    if (req.isDir) return res.send(405, 'Path is a directory')
+    
+    await fs.promise.truncate(req.filePath, 0)    
+    if(!req.isDir) req.pipe(fs.createWriteStream(req.filePath))
+    res.end()
+  }().catch(next)
+})
+
+function setDirDetails(req, res, next) {
+  let filePath = req.filePath
+  let endsWithSlash = filePath.charAt(filePath.length - 1) === path.sep
+  let hasExt = path.extname(filePath) !== ''
+  req.isDir = endsWithSlash || !hasExt
+  req.dirPath = req.isDir ? filePath : path.dirname(filePath)
+  next()
+}
 
 /* rather than splitting into separate functions, the 
 calls are split into middlewares and then called*/
@@ -69,14 +111,3 @@ function sendHeaders(req, res, next) {
   }(), next)
   //.catch(next) //next is called only if this promise results in an error
 }
-
-app.delete('*', setFileMeta, (req, res, next) => {
-  async () => {
-    if (!req.stat) return res.send(400, 'Invalid Path')
-    
-    if (req.stat.isDirectory()) {
-     await rimraf.promise(req.filePath)
-    } else await fs.promise.unlink(req.filePath)
-    res.end()
-  }().catch(next) 
-})
