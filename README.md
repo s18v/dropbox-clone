b78ffe6644e9ec6e4fd4938552dc395c48c3dd34
Saran <sarankumarvellanki@gmail.com>
2015-05-04 23:13:10 -0700

    add TCP

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dropbox-clone
+<p>This is a basic Dropbox clone to sync files across multiple remote folders.</p>
+
+<p>Time spent: <code>13</code></p>
+
+<h3><a class="anchor" id="heading-features" href="#heading-features"><span class="glyphicon glyphicon-link"></span></a>Features</h3>
+
+<h4><a class="anchor" id="heading-required" href="#heading-required"><span class="glyphicon glyphicon-link"></span></a>Completed</h4>
+
+<ul>
+<li>GET requests to get file or directory contents</li>
+<li>HEAD request to get just the GET headers </li>
+<li>PUT requests to create new directories and files with content</li>
+<li>POST requests to update the contents of a file</li>
+<li>ELETE requests to delete files and folders</li>
+<li>Server will serve from <code>--dir</code> or cwd as root</li>
+<li>Client will sync from server over TCP to cwd or CLI <code>dir</code> argument</li>
+</ul>
--- index.js
+++ index.js
@@ -7,30 +7,38 @@ let mime = require('mime-types')
 let rimraf = require('rimraf')
 let mkdirp = require('mkdirp')
 let argv = require('yargs').argv
+let net = require('net')
+let JsonSocket = require('json-socket')
 // songbird works during runtime. Hence, not assigning it to a variable
 require('songbird')
 
 const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 8000;
 // need a root directory for the file system api implementation
-const ROOT_DIR = path.resolve(process.cwd());
+const ROOT_DIR = argv.dir ? path.resolve(argv.dir) : path.resolve(process.cwd());
 
 // setting up the express app
 let app = express()
 
 // use() method is for adding the middleware 
- app.use(morgan('dev'))
-if(NODE_ENV === 'development') {
- 
-}
+app.use(morgan('dev'))
 
-app.listen(PORT, () => console.log(`Listening @ http://127.0.0.1:${PORT}`))
+// TCP Functionality
+let TCP_PORT = '6789'
+let TCP_HOST = '127.0.0.1'
+let server = net.createServer()
+server.on('connection', function (socket) {
+  socket = new JsonSocket(socket);
+})
+server.listen(TCP_PORT)
+
+app.listen(PORT,() => console.log(`Listening @ http://127.0.0.1:${PORT}`))
 
 // HEADERS
-app.head('*', setFileMeta, sendHeaders, (req, res) => res.end())
+app.head('*', setFileMeta, sendHeaders,(req, res) => res.end())
 
 // GET
-app.get('*', setFileMeta, sendHeaders, (req, res) => {
+app.get('*', setFileMeta, sendHeaders,(req, res) => {
   if (res.body) {
     res.json(res.body)
     return
@@ -39,37 +47,37 @@ app.get('*', setFileMeta, sendHeaders, (req, res) => {
 })
 
 // DELETE
-app.delete('*', setFileMeta, (req, res, next) => {
-  async () => {
+app.delete('*', setFileMeta,(req, res, next) => {
+  async() => {
     if (!req.stat) return res.send(400, 'Invalid Path')
-    
+
     if (req.stat.isDirectory()) {
-     await rimraf.promise(req.filePath)
+      await rimraf.promise(req.filePath)
     } else await fs.promise.unlink(req.filePath)
     res.end()
-  }().catch(next) 
+  } ().catch(next)
 })
 
 // PUT
-app.put('*', setFileMeta, setDirDetails, (req, res, next) => {
-  async () => {
+app.put('*', setFileMeta, setDirDetails,(req, res, next) => {
+  async() => {
     if (req.stat) return res.send(405, 'File Exists')
     await mkdirp.promise(req.dirPath)
-    if(!req.isDir) req.pipe(fs.createWriteStream(req.filePath))
+    if (!req.isDir) req.pipe(fs.createWriteStream(req.filePath))
     res.end()
-  }().catch(next)
+  } ().catch(next)
 })
 
 // POST
-app.post('*', setFileMeta, setDirDetails, (req, res, next) => {
-  async () => {
+app.post('*', setFileMeta, setDirDetails,(req, res, next) => {
+  async() => {
     if (!req.stat) return res.send(405, 'File does not exist')
     if (req.isDir) return res.send(405, 'Path is a directory')
-    
-    await fs.promise.truncate(req.filePath, 0)    
-    if(!req.isDir) req.pipe(fs.createWriteStream(req.filePath))
+
+    await fs.promise.truncate(req.filePath, 0)
+    if (!req.isDir) req.pipe(fs.createWriteStream(req.filePath))
     res.end()
-  }().catch(next)
+  } ().catch(next)
 })
 
 function setDirDetails(req, res, next) {
@@ -87,32 +95,32 @@ function setFileMeta(req, res, next) {
   // ROOT_DIR and __dirname are the same
   // resolve() resolves the path of any '.' characters
   req.filePath = path.resolve(path.join(ROOT_DIR, req.url))
-  if(req.filePath.indexOf(ROOT_DIR) !== 0) {
-     res.send(400, 'Invalid Path')
-     return
-   }
+  if (req.filePath.indexOf(ROOT_DIR) !== 0) {
+    res.send(400, 'Invalid Path')
+    return
+  }
   // Way to call nodeify on a promise; check for async function too
   fs.promise.stat(req.filePath)
-    .then(stat => req.stat = stat, () => req.stat = null)
+    .then(stat => req.stat = stat,() => req.stat = null)
     .nodeify(next)
 }
 
 function sendHeaders(req, res, next) {
   // promise is connected to the next callback (use of nodeify)
   // nodeify calls next even if it succeeds or fails
-  nodeify(async () => {
+  nodeify(async() => {
     // If the path is a directory, return the contents
     if(req.stat.isDirectory()) {
       let files = await fs.promise.readdir(req.filePath)
       res.body = JSON.stringify(files)
       res.setHeader('Content-Length', res.body.length)
       res.setHeader('Content-Type', 'application/json')
-      return 
+      return
     }
     
     res.setHeader('Content-Length', req.stat.size)
     let contentType = mime.contentType(path.extname(req.filePath))
-    res.setHeader('Content-Type', contentType)   
+    res.setHeader('Content-Type', contentType)
   }(), next)
   //.catch(next) //next is called only if this promise results in an error
 }
